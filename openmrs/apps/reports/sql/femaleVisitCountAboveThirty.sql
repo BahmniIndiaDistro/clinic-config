2e54556cb0ea50f1d7b2828e588a1437baa4dbea
Rahul Ramesh <121226043+rahu1ramesh@users.noreply.github.com>
2023-06-05 20:24:56 +0530

    118 | 119 | Female OPD cases above 30 years age group (#15)
    * [Rahul] | 118 | Add. Visit Count For Patients Above Thirty
    
    * [Rahul] | 119 | Add. Visit Count For Female Patients Above Thirty
    
    * [Rahul] | 119 | Fix. Report Names For OPD Visits
    
    * [Rahul] | 119 | Fix. NULL Value Check
    
    ---------
    
    Co-authored-by: Gokul Kuppan <104197616+gokultw@users.noreply.github.com>

--- openmrs/apps/reports/sql/femaleVisitCountAboveThirty.sql
+++ openmrs/apps/reports/sql/femaleVisitCountAboveThirty.sql
@@ -1,8 +1,8 @@
 SELECT
-    SUM(visit_type_name = 'OPD New')                                                                             AS `Total number of OPD cases`,
-    SUM(age > 30)                                                                                                AS `Number of OPD cases above the age of 30`,
-    SUM(age > 30 and gender = 'F')                                                                               AS `Number of Female OPD cases above the age of 30`,
-    CONCAT(ROUND((SUM(age > 30 and gender = 'F') / NULLIF(SUM(visit_type_name = 'OPD New'), 0) * 100), 2), '%')  AS `Percentage of Female OPD cases above the age of 30`
+    IFNULL(SUM(visit_type_name LIKE 'OPD%'),0)                                                                                                             AS `Total number of OPD cases`,
+    IFNULL(SUM(age > 30 AND visit_type_name LIKE 'OPD%'),0)                                                                                                AS `Number of OPD cases above the age of 30`,
+    IFNULL(SUM(age > 30 AND visit_type_name LIKE 'OPD%' AND gender = 'F'),0)                                                                               AS `Number of Female OPD cases above the age of 30`,
+    CONCAT(ROUND((IFNULL(SUM(age > 30 AND visit_type_name LIKE 'OPD%' AND gender = 'F'),0) / IFNULL(SUM(visit_type_name LIKE 'OPD%'), 1) * 100), 2), '%')  AS `Percentage of Female OPD cases above the age of 30`
 FROM
     (
         SELECT
@@ -14,6 +14,6 @@ FROM
             INNER JOIN person p ON p.person_id = v.patient_id
             INNER JOIN visit_type vt ON vt.visit_type_id = v.visit_type_id
         WHERE
-            CONVERT_TZ(v.date_started, '+00:00', '+5:30') BETWEEN '#startDate#' AND '#endDate#'
+            CAST(CONVERT_TZ(v.date_started,'+00:00','+05:30') AS DATE) BETWEEN '#startDate#' AND '#endDate#'
     ) 
 AS raw_result;
--- openmrs/apps/reports/sql/visitCountAboveThirty.sql
+++ openmrs/apps/reports/sql/visitCountAboveThirty.sql
@@ -1,7 +1,7 @@
 SELECT
-    SUM(visit_type_name = 'OPD New')                                                            AS `Total number of OPD cases`,
-    SUM(age > 30)                                                                               AS `Number of OPD cases above the age of 30`,
-    CONCAT(ROUND((SUM(age > 30) / NULLIF(SUM(visit_type_name = 'OPD New'), 0) * 100), 2), '%')  AS `Percentage of OPD cases above the age of 30`
+    IFNULL(SUM(visit_type_name LIKE 'OPD%'),0)                                                                                            AS `Total number of OPD cases`,
+    IFNULL(SUM(age > 30 AND visit_type_name LIKE 'OPD%'),0)                                                                               AS `Number of OPD cases above the age of 30`,
+    CONCAT(ROUND((IFNULL(SUM(age > 30 AND visit_type_name LIKE 'OPD%'),0) / IFNULL(SUM(visit_type_name = 'OPD New'), 1) * 100), 2), '%')  AS `Percentage of OPD cases above the age of 30`
 FROM
     (
         SELECT
@@ -12,6 +12,6 @@ FROM
             INNER JOIN person p ON p.person_id = v.patient_id
             INNER JOIN visit_type vt ON vt.visit_type_id = v.visit_type_id
         WHERE
-            CONVERT_TZ(v.date_started, '+00:00', '+5:30') BETWEEN '#startDate#' AND '#endDate#'
+            CAST(CONVERT_TZ(v.date_started,'+00:00','+05:30') AS DATE) BETWEEN '#startDate#' AND '#endDate#'
     ) 
 AS raw_result;
